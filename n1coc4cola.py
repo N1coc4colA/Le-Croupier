49ce07f36e9c3fec14f1ddf7e98c43aeeccfb832
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2020-09-08 09:13:21 -1000

    Update n1coc4cola.py

--- n1coc4cola.py
+++ n1coc4cola.py
@@ -8,7 +8,16 @@ Created on Tue Sep  8 08:31:54 2020
 
 mise=0
 argent=5000
-
+def helpme():
+    print("Nous allons vous expliquer les règles!")
+    print("Nous allons jouer à la roulette\n")
+    print("La roulette est composée de 50 cases, des cases numérotées de 0 à 49 et de couleurs rouges et noires")
+    print("Les cases noires seront les cases paires et les cases rouges seront les cases impaires\n")
+    print("Je vais faire tourner la roulette avec une seule bille, si la bille tombe sur le numéro que vous avez misé,")
+    print("Vous gagnez le triple de votre mise, mais si ce n'est pas le cas,")
+    print("Nous allons regarder si la bille tombe sur la couleur que vous avez misée, et si c'est le cas,")
+    print("Uniquement la moitié de votre mise vous sera rendu, en cas contraire vous la perdez\n")
+    print("Les présentations du jeu sont faites, nous pouvons jouer")
     
 def getValues():
     m=input("Quelle somme souhaitez vous miser? Entrez la valeur: ")
@@ -35,7 +44,7 @@ def showValues(firstTime = False):
     askContinue()
 
 def main():
-    """helpme() exec func"""
+    helpme()
     showValues(True)
     
 main()

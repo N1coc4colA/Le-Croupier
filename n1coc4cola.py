c8f07100cb96389424737c508cf4b0b40c495f7e
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2020-09-10 18:49:00 +1400

    Fix var issues, add core function, fix loop
    Changed the model to a loop, now it is more flexible. Py's in-func var aren't shared, fix with new file ["variables.py"]. "roulette" func have been added (core function, else there's no game)!

--- n1coc4cola.py
+++ n1coc4cola.py
@@ -2,49 +2,59 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep  8 08:31:54 2020
-
 @author: @n1coc4cola
 """
 
-mise=0
-argent=5000
-def helpme():
-    print("Nous allons vous expliquer les règles!")
-    print("Nous allons jouer à la roulette\n")
-    print("La roulette est composée de 50 cases, des cases numérotées de 0 à 49 et de couleurs rouges et noires")
-    print("Les cases noires seront les cases paires et les cases rouges seront les cases impaires\n")
-    print("Je vais faire tourner la roulette avec une seule bille, si la bille tombe sur le numéro que vous avez misé,")
-    print("Vous gagnez le triple de votre mise, mais si ce n'est pas le cas,")
-    print("Nous allons regarder si la bille tombe sur la couleur que vous avez misée, et si c'est le cas,")
-    print("Uniquement la moitié de votre mise vous sera rendu, en cas contraire vous la perdez\n")
-    print("Les présentations du jeu sont faites, nous pouvons jouer")
+import variables
+import math
+from random import randrange
+
+def roulette():
+    num = randrange(0, 50, 1)+1
+    print("Le numéro sorti est le ", num)
+    if (num == variables.numero):
+        variables.argent+=mise*3
+    elif (num%2 == variables.numero%2):
+        variables.argent+=math.ceil(variables.mise*0.5)
+        print("Vous avez gagné ", math.ceil(variables.mise*0.5), ", vous ferez mieux la prochaine fois!")
+    else:
+        variables.argent-=variables.mise
+        print("Vous avez perdu, essayez encore, la chance vous sourira peut-être!")
+
     
 def getValues():
-    m=input("Quelle somme souhaitez vous miser? Entrez la valeur: ")
-    mise=int(m)
-    print("Sam: vous avez misé: ", mise, ", Je lance la roulette!")
-    """roulette() exec func"""
+    """Use .isDigit() to check that it is only numbers"""
+    variables.mise = int(input("Quelle somme souhaitez vous miser? Entrez la valeur: "))
+    variables.numero = int(input("Vous voulez miser sur quel numéro? De 1 à 50!"))
+    print("Sam: vous avez misé: ", variables.mise, "sur le numéro ", variables.numero, ". Je lance la roulette!")
+    """When we'll use digit checks, leave it here will be useful."""
+    roulette()
 
 
 def askContinue():
     c=input("Voulez-vous continuer? [O/N]")
     if (c[0] == ("o" or "O")):
-        getValues()
+        return True
     elif(c[0] == ("n" or "N")):
-        print("Sam: Au plaisir de vous revoir dans notre casino!")
+        return False
     else:
         print("Hey! Ne vous endormez pas, l'argent n'attend pas!")
-        askContinue()
+        return askContinue()
 
 
 def showValues(firstTime = False):
-    print("Sam: Pour l'instant vous avez: ", argent);
-    if firstTime == False:
-        print("Votre dernière mise était de: ", mise)
-    askContinue()
+    print("Sam: Pour l'instant vous avez: ", variables.argent, "XPF");
+    if variables.isFirstTime == False:
+        print("Votre dernière mise était de: ", variables.mise, "XPF")
 
 def main():
-    helpme()
-    showValues(True)
+    variables.helpme()
+    while variables.wantsToContinue == True:
+        showValues()
+        if askContinue() == True:
+            getValues()
+        else:
+            variables.wantsToContinue = False
+    print("Sam: Au plaisir de vous revoir dans notre casino!")
     
 main()
